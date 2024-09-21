778743b4dd97871ca5fbf56ee2af78e98f54d958
Sardorbek <sardorbekmusilman@gmail.com>
2024-09-21 11:45:00 +0500

    sjjs

--- src/modules/users/showMe.js
+++ src/modules/users/showMe.js
@@ -2,12 +2,7 @@ import Users from './Users.js';
 
 export const showDataMe = async (req) => {
   const { id } = req.user;
-  const data = await Users.findById(id).populate([
-    'total_guides',
-    'todo_guides',
-    'read_guides'
-  ]);
-
+  const data = await Users.findById(id)
   if (!data) {
     throw new NotFoundError(` User (${id}) not found`);
   }
